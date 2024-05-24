f2c422928ff41c67f90560010dfc52b3d1f2e6e0
kurdt23 <organismus22@gmail.com>
2024-05-24 22:14:11 +0500

    feat: Исправление стиля перед разделением  кода на отдельные модули -	Исправлена стилистика когда (лишние/недостающие переменные и т.д.) -	Добавлены комментарии к модулям -	Упрощены некоторые функции в соответствии с новыми переменными

--- main.py
+++ main.py
@@ -40,20 +40,11 @@ def compute_overlaps(boxes1, boxes2):
 
 
 # Отрисовка рамок
-def draw_bbox(x, y, w, h, parking_text, parking_color=(0, 0, 255)):
-    start = (x, y)
-    end = (x + w, y + h)
-    color = parking_color
-    width = 2
-    final_image = cv2.rectangle(image_to_process, start, end, color, width)
-
-    start = (x, y - 10)
-    font_size = 0.4
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    width = 1
-    text = parking_text
-    final_image = cv2.putText(final_image, text, start, font, font_size, color, width, cv2.LINE_AA)
-    return final_image
+def draw_bbox(image, x, y, w, h, text, color=(0, 0, 255)):
+    start, end = (x, y), (x + w, y + h)
+    cv2.rectangle(image, start, end, color, 2)
+    cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 1, cv2.LINE_AA)
+    return image
 
 
 # Отправка сообщения в телеграм
@@ -67,36 +58,29 @@ def send_photo(token, chat_id, img_path):
     requests.post(f'https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}', files=files)
 
 
-# Парковочные места
+video_capture = cv2.VideoCapture(video_path)
 check_det_frame = None
+final_image = 0
 first_frame_parking_spaces = None
-first_parking_timer = 0
-free_parking_count = 0
 free_parking_timer = 0
 free_parking_timer_bag1 = 0
+first_frame_parking_score = 0
 free_parking_space = False
 free_parking_space_box = None
-
-# Сообщение в телеграм
 telegram_message = False
+x_free, y_free, w_free, h_free = 0, 0, 0, 0
 
-video_path = './video.mp4'
-
-video_capture = cv2.VideoCapture(video_path)
-
-# Пока не нажата клавиша q функция будет работать
-# isOpened() Возвращает true, если захват видео уже инициализирован.
+# Работа модели с отображением результатов на видео, пока не нажата клавиша q
 while video_capture.isOpened():
     ret, image_to_process = video_capture.read()
 
     height, width, _ = image_to_process.shape
 
+    # Определение класса машин ("2") в COCO
     results = model.track(image_to_process, persist=False)
-
-    frame_ = results[0].plot()
-
+    detections = results[0].boxes
     class_indexes, class_scores, boxes = ([] for i in range(3))
-    for class_index in results[0].boxes:
+    for class_index in detections:
         if class_index.cls.numpy() == 2:
             box = class_index.xywh.numpy().astype(int)[0].tolist()
             box = [box[0] - box[2] // 2, box[1] - box[3] // 2,
@@ -104,64 +88,64 @@ while video_capture.isOpened():
             boxes.append(box)
             class_scores.append(float(class_index.conf))
 
+    # Предполагаем, что под каждой стоячей машиной будет парковочное место
     if not first_frame_parking_spaces:
-        # Предполагаем, что под каждой машиной будет парковочное место
         first_frame_parking_spaces = boxes
         first_frame_parking_score = class_scores
-
 
     else:
         chosen_cars_boxes = cv2.dnn.NMSBoxes(boxes, class_scores, 0.0, 0.4)
         cars_area = []
-        # МАШИНЫ
+
+        # Распознавание машин
         for box_index in chosen_cars_boxes:
             car_box = boxes[box_index]
             cars_area.append(car_box)
 
             x, y, w, h = car_box
             parking_text = 'Car'
-            final_image = draw_bbox(x, y, w, h, parking_text, (255, 255, 0))
+            final_image = draw_bbox(image_to_process, x, y, w, h, parking_text, (255, 255, 0))
         cars_boxes = cars_area
 
         # IoU
-        overlaps = compute_overlaps(np.array(parking_spaces), np.array(cars_boxes))
+        overlaps = compute_overlaps(np.array(first_frame_parking_spaces), np.array(cars_boxes))
 
-        for parking_space_one, area_overlap in zip(parking_spaces, overlaps):
+        for parking_space_one, area_overlap in zip(first_frame_parking_spaces, overlaps):
 
             max_IoU = max(area_overlap)
             sort_IoU = np.sort(area_overlap[area_overlap > 0])[::-1]
 
             if not free_parking_space:
                 if 0.0 < max_IoU < 0.4:
-                    # Количество паркомест по условию 1: 0.0 < IoU < 0.4
+
+                    # Количество парковочных мест по первому условию: 0.0 < IoU < 0.4
                     len_sort = len(sort_IoU)
 
-                    # Количество паркомест по условию 2: IoU > 0.15
+                    # Количество парковочных мест по второму условию: IoU > 0.15
                     sort_IoU_2 = sort_IoU[sort_IoU > 0.15]
                     len_sort_2 = len(sort_IoU_2)
 
-                    # Смотрим чтобы удовлятворяло условию 1 и условию 2
+                    # Проверка двух условий для подсчета кадров с освободившимся местом
                     if (check_det_frame == parking_space_one) & (len_sort != len_sort_2):
-                        # Начинаем считать кадры подряд с пустыми координатами
                         free_parking_timer += 1
 
                     elif check_det_frame is None:
                         check_det_frame = parking_space_one
 
                     else:
-                        # Фильтр от чехарды мест (если место чередуется, то "скачет")
+                        # Фильтр от багов со "скачком" свободного парковочного места
                         free_parking_timer_bag1 += 1
                         if free_parking_timer_bag1 == 2:
-                            # Обнуляем счётчик, если паркоместо "скачет"
+                            # Обнуление счётчика
                             check_det_frame = parking_space_one
                             free_parking_timer = 0
 
-                    # Если более 10 кадров подряд, то предполагаем, что место свободно
+                    # Условие освободившегося места (10 кадров подряд)
                     if free_parking_timer == 10:
                         # Помечаем свободное место
                         free_parking_space = True
                         free_parking_space_box = parking_space_one
-                        # Отрисовываем рамку парковочного места
+                        # Отрисовка рамки освободившегося парковочного места
                         x_free, y_free, w_free, h_free = parking_space_one
 
             else:
@@ -189,14 +173,14 @@ while video_capture.isOpened():
 
     for box_index in chosen_boxes:
         box = first_frame_parking_spaces[box_index]
-        # Если определилось пустое место, то отрисуем его в кадре
+        # Отрисовка освободившегося места
         if free_parking_space:
 
             if box == [x_free, y_free, w_free, h_free]:
                 parking_text = 'FREE SPACE!!!'
-                final_image = draw_bbox(x_free, y_free, w_free, h_free, parking_text, (0, 255, 0))
+                final_image = draw_bbox(image_to_process, x_free, y_free, w_free, h_free, parking_text, (0, 255, 0))
 
-                # Отправка сообщения телеграмм-ботом о свободном мест
+                # Отправка сообщения телеграмм-ботом о свободном месте
                 if not telegram_message:
                     # Скриншот свободного места
                     screenshot_parking_space = final_image
@@ -208,23 +192,23 @@ while video_capture.isOpened():
 
                     telegram_message = True
             else:
+                # Координаты и размеры парковочного места
                 x, y, w, h = box
                 parking_text = 'No parking'
-                final_image = draw_bbox(x, y, w, h, parking_text)
-
+                final_image = draw_bbox(image_to_process, x, y, w, h, parking_text)
 
         else:
-            # Координаты и размеры BB
+            # Координаты и размеры парковочного места
             x, y, w, h = box
             parking_text = 'No parking'
-            final_image = draw_bbox(x, y, w, h, parking_text)
+            final_image = draw_bbox(image_to_process, x, y, w, h, parking_text)
 
-        # Координаты парковочных мест с первого кадры
+        # Координаты парковочных мест, обнаруженных на первом кадре
         parking_spaces.append(box)
-    # Показать результат работы
+    # Отображение результатов работы модели
     cv2.imshow("Parking Space", final_image)
 
-    # Прерывание работы клавишей q
+    # Прерывание отображения клавишей q
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
