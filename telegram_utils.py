85431e3e56080f8835c7168ed9651af6f10f1433
kurdt23 <organismus22@gmail.com>
2024-05-25 01:25:21 +0500

    refactor: Улучшение организации кода - Разделение кода на модули для улучшения читаемости и повторного использования. - Перенос функций детекции в detection.py. - Перенос функций отправки сообщений в telegram_utils.py. - Основной скрипт теперь main.py.
    Разделение кода на модули улучшает его структуру и делает проект более поддерживаемым и расширяемым.

---
+++ telegram_utils.py
@@ -0,0 +1,12 @@
+import requests
+
+
+# Отправка сообщения в телеграм
+def send_message(token, chat_id, message):
+    requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}')
+
+
+# Отправка фото в телеграм
+def send_photo(token, chat_id, img_path):
+    files = {'photo': open(img_path, 'rb')}
+    requests.post(f'https://api.telegram.org/bot{token}/sendPhoto?chat_id={chat_id}', files=files)
